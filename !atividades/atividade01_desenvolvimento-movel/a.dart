2ac847b60a84f631a76d6a1e70afaf1151a3c3b6
colynascimento <colymsnascimento@gmail.com>
2025-10-11 23:43:39 -0300

    atividade01 a

---
+++ !atividades/atividade01_desenvolvimento-movel/a.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+import 'dart:async';
+import 'utils/firulas.dart';
+
+Future<void> main() async {
+
+    while (true) {
+        stdout.write('Digite "Olá Mundo!": ');
+
+        String? mensagem = stdin.readLineSync();
+
+        // if mensagem for null, usa '' (string vazia)
+        String mensagemNormalizada = normalizarTexto(mensagem ?? '');
+
+        if (mensagemNormalizada.toLowerCase() == 'ola mundo!') {
+            print('Obrigada!')
+            await finalizarPrograma();
+            break;
+        } else {
+            print('Mensagem incorreta! Tente novamente.');
+        }
+    }
+}
